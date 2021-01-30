1efa03b087b4934a1b8bf65fbe8f68fcc2ea0e08
mamil <mamilgithub@gmail.com>
2021-01-30 21:59:34 +0800

    add route /user /echo

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "github.com/gin-gonic/gin"
 
+type User struct {
+	ID   uint64
+	Name string
+}
+
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
@@ -9,5 +14,18 @@ func main() {
 			"name":"gin-note",
 		})
 	})
+
+	// user
+	users := []User{{ID:1,Name:"tom"}, {ID:2,Name:"tim"}}
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(200, users)
+	})
+
+	// echo
+	r.GET("/echo/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		c.String(200, id)
+	})
+
 	r.Run(":8080")
 }
